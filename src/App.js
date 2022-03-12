73070983b4fdc6d11223f6f0812507926c1195e0
abbasi-naim <abbasi.naim@gmail.com>
2022-03-12 18:38:41 +0300

    commit app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import logo from "./logo.png";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">React Dictionary</header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={logo}
+            className="App-logo
+            img-fluid"
+            alt="logo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Naim Abbasi</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
