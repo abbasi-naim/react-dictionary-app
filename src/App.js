25ced19980b9746010f86b24f5f14ecb147639cb
abbasi-naim <abbasi.naim@gmail.com>
2022-03-13 20:27:40 +0300

    styled dictionary and results components

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="react" />
         </main>
         <footer className="App-footer">
           <small>
